7cc627bba29c1c8684e30e5a491b17b07a018282
Juan Reyes <jr.reyes289@uniandes.edu.co>
2021-02-15 21:40:33 -0500

    update until video 19 using conditions in a template
    update until video 19 using conditions in a template

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,13 +6,19 @@ def home_view(request, *args, **kwargs):
     print(*args, **kwargs)
     print(request)
     print(request.user)
-    return HttpResponse("<h1>Hello world</h1>") # string of html code
+    #return HttpResponse("<h1>Hello world</h1>") # string of html code
+    return render(request,"home.html",{})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>contact page</h1>") # string of html code    
+    return render(request,"contact.html",{}) # string of html code    
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>about page</h1>") # string of html code    
+    my_context = {
+        "my_text":"This is about us",
+        "my_number":124,
+        "my_list":[1,2,3,4]
+    }
+    return render(request,"about.html",my_context) # string of html code    
 
-def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>contact page</h1>") # string of html code        
+def Social_view(request, *args, **kwargs):
+    return render(request,"about.html",{}) # string of html code    
